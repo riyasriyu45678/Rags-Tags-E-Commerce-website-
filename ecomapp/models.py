b54efee4ba94b6d78623fcfe0fae910b84b955a5
riyas riyu <riyasriyu45678@gmail.com>
2022-12-14 12:38:10 +0530

    Updated

--- ecomapp/models.py
+++ ecomapp/models.py
@@ -14,9 +14,11 @@ class Product(models.Model):
     name = models.CharField(max_length=200)
     price = models.FloatField()
     digital = models.BooleanField(default=False, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.name
+
 
 class Order(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
