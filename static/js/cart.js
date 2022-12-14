0edf37bd648bda37f3f8b32e1029a5c4df18468e
riyas riyu <riyasriyu45678@gmail.com>
2022-12-14 23:11:06 +0530

    fetching done

--- static/js/cart.js
+++ static/js/cart.js
@@ -11,7 +11,28 @@ for (var i = 0; i < updateBtns.length; i++) {
         if (user === 'AnonymousUser'){
             console.log('Not logged in')
         }else{
-            console.log('User is logged in, sending data...')
+            updateUserOrder(productId, action)/* passing the function updateuserorder so it gets called when the user is logged */
         }
+    })
+}
+
+/* defining the function with 2 parameters : productId, action  */
+function updateUserOrder(productId, action){
+    console.log('User is logged, sending data...')
+
+    var url = '/update_item/'
+/* create and use the fetch API to sent a post request */
+    fetch(url, {
+        method:'POST',
+        headers:{
+            'Content-Type':'application/json',
+        },
+        body:JSON.stringify({'productId':productId, 'action':action}) /* converting and sending our productId and action as a json object */
+    })
+    .then((responce) => {
+        return responce.json();
+    })
+    .then((data) =>{
+        console.log('data:',data)
     })
 }
